21e1ef956c74da1d7545dffc736b723fdad00c59
jparcar <115104686+jparcar@users.noreply.github.com>
2023-02-06 18:42:26 +0100

    ADD- Control  multiplayer of players.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ public enum GameState
 {
     Playing,
     Paused,
-    GameOver
+    GameOver,
     Victory
 }
 
---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerControl : NetworkBehaviour
+{
+    [SerializeField]
+    private float walkSpeed = 0.1f;
+
+
+
+    [SerializeField]
+    private NetworkVariable<float> forwardBackPosition = new();
+
+    [SerializeField]
+    private NetworkVariable<float> leftRightPosition = new();
+
+
+
+
+
+
+    private Rigidbody2D rigBody;
+
+
+
+
+    private void Start()
+    {
+
+        rigBody = GetComponentInParent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+
+        if (IsServer)
+        {
+            UpdateServer();
+        }
+
+        if (IsClient && IsOwner)
+        {
+            UpdateClient();
+        }
+
+    }
+
+    private void UpdateServer()
+    {
+        transform.position = new Vector3(transform.position.x + leftRightPosition.Value, transform.position.y,
+            transform.position.z + forwardBackPosition.Value);
+    }
+
+    private void UpdateClient()
+    {
+        this.transform.rotation = Quaternion.identity;
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector2(x, y);
+
+        //rigBody.AddForce(force * walkSpeed);
+        // update the server
+        UpdateClientPositionServerRpc(force, walkSpeed);
+
+    }
+
+    [ServerRpc]
+    private void UpdateClientPositionServerRpc(Vector3 direction, float walkSpeed)
+    {
+        rigBody.velocity = direction* walkSpeed;
+    }
+}
